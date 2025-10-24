186d4c16fb25add3b875fc9a861bc19bb0a01db4
Nirmal <nirmalmhjr@users.noreply.github.com>
2025-10-24 18:36:12 +0545

    feat: product Viewer created with packages for 3 rendering

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+const useMacbookStore = create((set) => ({
+  color: "#2e2c2e",
+  scale: 0.08,
+  setColor: (color) => set({ color }),
+  setScale: (scale) => set({ scale }),
+  reset: () => set({ color: "#2e2c3e", scale: 0.08 }),
+}));
+
+export default useMacbookStore;
